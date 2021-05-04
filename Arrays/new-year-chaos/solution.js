f85ab591e20010c62732f226cc14e271fa5543b0
Scott Taylor <scott.c.taylor@mac.com>
2021-05-03 22:42:17 -0400

    Update solution.js

--- Arrays/new-year-chaos/solution.js
+++ Arrays/new-year-chaos/solution.js
@@ -1,22 +1,29 @@
 // the solution is faster because it can avoid
 // all array manipulation methods, since no array is actually returned
+// and the array itself does not affect the outcome
 function minimumBribes(q) {
-  if (q.length < 2) {
-    return '';
-  }
-
   let total = 0;
+  // track the minimum value
+  // this works, but I can't remember why this is the approach to use?
   let min = q.length;
+
+  // reverse loops are faster :shrug:
   for (let i = q.length - 1; i >= 0; i -= 1) {
-    const current = q[i];
-    if (current - i > 3) {
+    // imagine: 1 2 3 4 5 10 6 7 8 9
+    if (q[i] - i > 3) {
       return 'Too chaotic';
     }
-    if (current > i + 1) {
-      total += current - (i + 1);
-    } else if (min > current) {
-      min = current;
-    } else if (current !== min) {
+    // current item is greater than it's index, meaning bribe(s) occurred
+    if (q[i] > i + 1) {
+      // subtract index (1-based, not 0) from value (1-based, not 0)
+      // to retrieve number of positions / bribes
+      total += q[i] - (i + 1);
+      // reset min to current value if it is greater
+    } else if (min > q[i]) {
+      min = q[i];
+      // if current value is not equal to min value
+      // increment number of bribes
+    } else if (q[i] !== min) {
       total += 1;
     }
   }
